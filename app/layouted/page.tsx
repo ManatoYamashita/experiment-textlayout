a38a66ad0277e0b8e556f64092ed5cba674b2173
yamashita <g2172117@tcu.ac.jp>
2024-10-09 14:15:42 +0900

    [add]fallback to sespense

--- app/layouted/page.tsx
+++ app/layouted/page.tsx
@@ -5,6 +5,7 @@ import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import TextLayout from '@/components/TextLayout';
 import styles from './page.module.scss';
+import Loading from '@/components/Loading';
 
 export default function LayoutPage() {
   const searchParams = useSearchParams();
@@ -22,9 +23,9 @@ export default function LayoutPage() {
   return (
     <div className={styles.layoutContainer}>
       <h1 className={styles.title}>✨Looks good?</h1>
-        <Suspense>
-          <TextLayout text={text} />
-        </Suspense>
+      <Suspense fallback={<div><Loading /></div>}>
+        <TextLayout text={text} />
+      </Suspense>
     </div>
   );
 }
