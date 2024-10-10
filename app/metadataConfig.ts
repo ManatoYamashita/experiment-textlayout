68122458380a8a35a348949e91549f2729fc3443
yamashita <g2172117@tcu.ac.jp>
2024-10-10 13:08:56 +0900

    [fix]ogp image path to /_next/ogp.png

--- app/metadataConfig.ts
+++ app/metadataConfig.ts
@@ -30,7 +30,7 @@ export const siteMetadata: Metadata = {
         description: 'プレーンテキストをDNPの研究に基づき再レイアウトするシステムです。',
         images: [
         {
-            url: 'ogp.png',
+            url: '/_next/ogp.png',
             width: 1200,
             height: 630,
             alt: '文章自動レイアウトWebApp',
